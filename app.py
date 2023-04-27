299441689f03ccd567c2283331ecc47e9d3611bc
amandalxllll <xliu7@babson.edu>
2023-04-27 10:34:27 -0400

    on going

--- app.py
+++ app.py
@@ -2,25 +2,32 @@
 A website using Flask which prompt user to enter
 stocks they hold, time they purchase, price/share they purchase,
 and the number of unit they purchased for each of the stock.
-return the corresponding Portfolio Return for the past week in a table.
+.
 """
 
 from flask import Flask, request, render_template
-import pandas as pd
 from returnportfolio import get_portfolio_return
-import json
 
-app=Flask(__name__)
+app = Flask(__name__)
 
-@app.route('/', methods = ['GET', 'POST'])
-def portfolio_return():
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/', methods=['GET', 'POST'])
+def portfolio():
     if request.method == 'POST':
+        tickers_symbols = request.form['tickers_symbols']
+        purchase_prices = request.form['purchase_prices']
+        amount_invested = request.form['amount_invested']
         try:
-            tickers_symbols = request.form['tickers_symbols']
-            purchase_prices = request.form["purchase_prices"]
-            amount_invested = request.form["amount_invested"]
-            result = get_portfolio_return(tickers_symbols,purchase_prices, amount_invested)
+            portfolio_return = get_portfolio_return(tickers_symbols, purchase_prices, amount_invested)
+            return render_template('returnportfolio.html', portfolio_return=portfolio_return)
+        except Exception as e:
+            print("error")
+            return render_template('error.html')
+    else:
+        return render_template('index.html')
 
-
-        except Exception:
-            return render_template("error.html")
+if __name__ == '__main__':
+    app.run(debug=True)
