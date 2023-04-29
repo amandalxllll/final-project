8830794d1d79c5760c642f3c3a3645c15729135c
amandalxllll <xliu7@babson.edu>
2023-04-29 18:37:42 -0400

    nearly finished

--- app.py
+++ app.py
@@ -1,27 +1,39 @@
-from flask import Flask, render_template, request
-from returnportfolio import get_portfolio_return
+"""
+A website using Flask which prompt user to enter
+stocks they hold, time they purchase, price/share they purchase,
+and the number of unit they purchased for each of the stock.
+.
+"""
 
+from flask import Flask, request, render_template
+from returnportfolio import get_portfolio_return
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template("index.html")
+# @app.route('/')
+# def index():
+#     return render_template('index2.html')
 
-@app.route("/", methods=["POST", "GET"])
-def stockreturn():
-    if request.method == "POST":
+@app.route('/', methods=['GET', 'POST'])
+def portfolio_return():
+    if request.method == 'POST':
+        # print('post')
         try:
-            tickers_symbols = request.form.getlist("tickers_symbols")
-            purchase_prices = request.form.getlist("purchase_prices")  # Update variable name
-            amount_invested = request.form.getlist['amount_invested']
+            # print(request.form.get('tickers_symbols'))
+            tickers_symbols = request.form.get('tickers_symbols').split()
+            purchase_prices = request.form.get('purchase_prices').split()
+            amount_invested = request.form.get('amount_invested').split()
+            purchase_prices = list(map(int, purchase_prices))
+            amount_invested = list(map(int, amount_invested))
+            print(tickers_symbols, purchase_prices, amount_invested)
             result = get_portfolio_return(tickers_symbols, purchase_prices, amount_invested)
+            print(result)
             return render_template('returnportfolio.html', result=result)
-        except Exception:
-            return render_template('error.html')
+        except Exception as e:
+            print(e)
+            return render_template('error.html') 
     else:
         return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
-
